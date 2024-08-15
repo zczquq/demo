3411a16ecbbd7ce0010176cdab804855ca219bed
zczquq <1633916572@qq.com>
2024-08-16 00:32:48 +0800

    antiguigu/

---
+++ antiguigu/springboot_jpa/src/test/java/org/example/study/springboot_jpa/SpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.study.springboot_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
